4a1fdad6aeacbeb78691bc5e3d6df32df637e816
foisal-hossen <farjana01811389904@gmail.com>
2022-10-28 19:50:36 +0600

    vercel added

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ const cors = require('cors');
 app.use(cors());
 
 const port = process.env.port || 5555;
-const pcd =require('./Data/course.json')
+const courseData =require('./Data/course.json')
 
 app.get('/', (req, res) => {
   res.send(' Server Choltache')
 });
 
-app.get('/product', (req, res) => {
-  res.send(' all products paisi')
-});
-
-app.get('/product/:id', (req, res) => {
-  
-  const id = req.params.id;
-  const getItem = pcd?.find((p) => p.id == id);
-  
-  if (!getItem) {
-    res.send('sorry bhaia khuje pelam na')
-  }
-
- res.send(getItem);
-  
+app.get('/courses', (req, res) => {
+  res.send(courseData);
 });
 
 
+// app.get('/course/:id', (req, res) => {
+  
+//   const id = req.params.id;
+//   const getItem = pcd?.find((display) => display.id == id);
+  
+//   if (!getItem) {
+//     res.send('sorry bhaia khuje pelam na')
+//   }
+
+//   res.send(getItem);
+// });
 
 app.listen(port, () => {
   console.log('server is runnig', port);
 })
+
+module.exports = app;
