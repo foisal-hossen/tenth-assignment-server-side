83add7bafc0c4fc8e440547b7d6b981b7d22bbf7
foisal-hossen <farjana01811389904@gmail.com>
2022-10-28 14:56:14 +0600

    file set

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.use(cors());
 
 const port = process.env.port || 5555;
 
+app.get('/', (req, res) => {
+  res.send(' Server Choltache')
+});
+
+app.get('/allproducts', (req, res) => {
+  res.send(' all products paisi')
+});
+
+
+
 app.listen(port, () => {
   console.log('server is runnig', port);
 })
