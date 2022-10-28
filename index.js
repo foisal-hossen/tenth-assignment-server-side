0c950d5dbd550e84b0f2ae1565381e4c64c314c5
foisal-hossen <farjana01811389904@gmail.com>
2022-10-28 16:02:52 +0600

    data added

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const cors = require('cors');
 app.use(cors());
 
 const port = process.env.port || 5555;
+const pcd =require('./Data/course.json')
 
 app.get('/', (req, res) => {
   res.send(' Server Choltache')
 });
 
-app.get('/allproducts', (req, res) => {
+app.get('/product', (req, res) => {
   res.send(' all products paisi')
+});
+
+app.get('/product/:id', (req, res) => {
+  
+  const id = req.params.id;
+  const getItem = pcd?.find((p) => p.id == id);
+  
+  if (!getItem) {
+    res.send('sorry bhaia khuje pelam na')
+  }
+
+ res.send(getItem);
+  
 });
 
 
